ac0077f242842a175ac1b9d1df7e73190a2c4469
MeonJiDust <sikong0685@gmail.com>
2022-06-25 23:03:11 +0900

    설탕배달 푸는 중..

---
+++ BaekJoon02/src/basicMath01/Num2839.java
@@ -0,0 +1,28 @@
+package basicMath01;
+
+import java.util.Scanner;
+
+public class Num2839 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		int sum = 0;
+		int remind = 0;
+		
+		while(true) {
+			
+			remind = num - 5;
+			if(remind % 5 == 0) {
+				continue;
+			}else if(remind % 3 == 0) {
+				
+			}
+		}
+
+	}
+
+}
