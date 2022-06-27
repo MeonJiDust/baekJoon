23d7a478b3466709a5dd93c5f4cb5d089bc67fee
MeonJiDust <sikong0685@gmail.com>
2022-06-27 15:29:58 +0900

    어거지로 풂 근데 맞았는데 뭐ㅎ

--- BaekJoon02/src/basicMath01/Num2839.java
+++ BaekJoon02/src/basicMath01/Num2839.java
@@ -10,18 +10,26 @@ public class Num2839 {
 		Scanner sc = new Scanner(System.in);
 		
 		int num = sc.nextInt();
-		int sum = 0;
-		int remind = 0;
+		int count = 0;
 		
-		while(true) {
+		if(num % 5 == 0) {
+			count = num / 5;
+		}else if(num % 5 == 1) {
+			count = ((num / 5) - 1) + 2;
 			
-			remind = num - 5;
-			if(remind % 5 == 0) {
-				continue;
-			}else if(remind % 3 == 0) {
-				
+		}else if(num % 5 == 2) {
+			count = ((num / 5) - 2) + 4;
+		}else if(num % 5 == 3) {
+			count = num / 5 + 1;
+		}else if(num % 5 == 4) {
+			count = ((num / 5) - 1) + 3;
+			if(num == 4) {
+				count = -1;
 			}
 		}
+		if(num == 4 || num == 7) count = -1;
+		
+		System.out.println(count);
 
 	}
 
