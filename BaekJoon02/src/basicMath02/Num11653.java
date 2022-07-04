9505ee430b68137e331932d03e7c56b35b3a6597
MeonJiDust <sikong0685@gmail.com>
2022-07-04 22:32:06 +0900

    소수는 쓸 때마다 알고리즘이 달라지네 ㅋ

---
+++ BaekJoon02/src/basicMath02/Num11653.java
@@ -0,0 +1,11 @@
+package basicMath02;
+
+public class Num11653 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	}
+
+}
--- BaekJoon02/src/basicMath02/Num2581.java
+++ BaekJoon02/src/basicMath02/Num2581.java
@@ -1,11 +1,62 @@
 package basicMath02;
 
+import java.util.Scanner;
+
 public class Num2581 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
 		
+		int m = sc.nextInt();
+		int n = sc.nextInt();
+		int sum = 0;
+		boolean first_min = true;
+		int min = 0;
+		
+		for(int i = m; i <= n; i++) {
+			
+			boolean isTrue = true;
+			
+			if(i == 2) {
+				sum += i;
+				if(first_min == true) {
+					min = i;
+					first_min = false;
+				}else {
+					if(i < min) {
+						min = i;
+					}
+				}
+				continue;
+			}else if(i % 2 == 0 || i == 1) {
+				continue;
+			}
+			for(int j = 3; j < i; j++) {
+				if(i % j == 0) {
+					isTrue = false;
+					break;
+				}
+			}
+			if(isTrue == true) {
+				sum += i;
+				if(first_min == true) {
+					min = i;
+					first_min = false;
+				}else {
+					if(i < min) {
+						min = i;
+					}
+				}
+			}
+		}
+		if(sum == 0) {
+			System.out.println(-1);
+		}else {
+			System.out.println(sum);
+			System.out.println(min);
+		}
 	}
 
 }
