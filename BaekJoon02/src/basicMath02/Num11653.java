0fcf62d54c1db995250d723ff954f3629e76f99e
MeonJiDust <sikong0685@gmail.com>
2022-07-05 20:34:48 +0900

    소인수분해

--- BaekJoon02/src/basicMath02/Num11653.java
+++ BaekJoon02/src/basicMath02/Num11653.java
@@ -1,11 +1,30 @@
 package basicMath02;
 
+import java.util.Scanner;
+
 public class Num11653 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
 		
+		int n = sc.nextInt();
+		
+		int i = 2;
+		
+		while(n > 0) {
+			
+			if(n == 1) {
+				break;
+			}
+			if(n % i == 0) {
+				n /= i;
+				System.out.println(i);
+			}else {
+				i++;
+			}
+		}
 	}
 
 }
