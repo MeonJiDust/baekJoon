a9cdd5807bb5d3546f8cfd3a55b2e703e752cc69
MeonJiDust <sikong0685@gmail.com>
2022-07-11 21:39:14 +0900

    재귀함수가 뭔가요? 푸는 중

---
+++ BaekJoon02/src/recursive/Num17478.java
@@ -0,0 +1,36 @@
+package recursive;
+
+import java.util.Scanner;
+/*
+ * ----이거 아직 해결 안 됨...
+ */
+
+public class Num17478 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		System.out.println(catbot(n));
+	}
+
+	private static String catbot(int n) {
+		// TODO Auto-generated method stub
+		if(n == 0) {
+			System.out.println("재귀함수가 뭔가요?");
+			System.out.println("재귀함수는 자기 자신을 호출하는 함수라네");
+			return "라고 답변하였지.\n";
+		}else {
+			System.out.println("재귀함수가 뭔가요?");
+			System.out.println("잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
+			System.out.println("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
+			System.out.println("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.");
+		}
+		
+		return catbot(n - 1) + "라고 답변하였지.\n";
+	}
+
+}
