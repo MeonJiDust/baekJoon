bc21877ac91982b06027912394cb12ffd3cc3175
MeonJiDust <sikong0685@gmail.com>
2022-07-12 22:07:19 +0900

    블랙잭 푸는 중

---
+++ BaekJoon02/src/bruteForce/Num2798.java
@@ -0,0 +1,25 @@
+package bruteForce;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Num2798 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+	}
+
+}
