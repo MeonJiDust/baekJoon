370d5d0878a50f785d8d3cb704b43da0fed1a033
MeonJiDust <sikong0685@gmail.com>
2022-07-22 18:43:24 +0900

    체스판(어떻게 이런 생각을..하지..ㅜ)

--- BaekJoon02/src/bruteForce/Num1018.java
+++ BaekJoon02/src/bruteForce/Num1018.java
@@ -6,6 +6,9 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Num1018 {
+	
+	public static boolean[][] arr;
+	public static int min = 64;
 
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
@@ -16,34 +19,56 @@ public class Num1018 {
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
 		
-		String[] arr = new String[n];
-		int black = 0;
-		int white = 0;
-		
-		int count = 0;
+		arr = new boolean[n][m];
 		
 		for(int i = 0; i < n; i++) {
 			
-			arr[i] = br.readLine();
+			String str = br.readLine();
 			
 			for(int j = 0; j < m; j++) {
 				 
-				if(arr[i].charAt(j) == 'B') {
-					black++;
+				if(str.charAt(j) == 'W') {
+					arr[i][j] = true;
 				}else {
-					white++;
+					arr[i][j] = false;
 				}
 			}
 		}
 		
-		if(black < 32) {
-			count += (32 - black);
-		}
-		if(white < 32) {
-			count += (32 - white);
+		int n_row = n - 7;
+		int m_col = m - 7;
+		
+		for(int i = 0; i < n_row; i++) {
+			for(int j= 0; j < m_col; j++) {
+				find(i, j);
+			}
 		}
 		
-		System.out.println(count);
+		System.out.println(min);
+	}
+
+	private static void find(int x, int y) {
+		// TODO Auto-generated method stub
+		int end_x = x + 8;
+		int end_y = y + 8;
+		int count = 0;
+		
+		boolean TF = arr[x][y];
+		
+		for(int i = 0; i < end_x; i++) {
+			for(int j = 0; j < end_y; j++) {
+				
+				if(arr[i][j] != TF) {
+					count++;
+				}
+				TF = (!TF);
+			}
+			TF = !TF;
+		}
+		
+		count = Math.min(count, 64 - count);
+		
+		min = Math.min(min, count);
 	}
 
 }
