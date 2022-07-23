ad979bb418bf5b5a13bafe095f48665e68d0429c
MeonJiDust <sikong0685@gmail.com>
2022-07-23 14:02:16 +0900

    영화감독 숌

---
+++ BaekJoon02/src/bruteForce/Num1436.java
@@ -0,0 +1,33 @@
+package bruteForce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Num1436 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int num = 666;
+		int count = 1;
+		
+		System.out.println("start");
+		
+		while(count != n) {
+			
+			num++;
+			
+			if(String.valueOf(num).contains("666")) {
+				System.out.printf("n: %d, num: %d, count: %d\n", n, num, count);
+				count++;
+			}
+		}
+		System.out.println(num);
+	}
+
+}
